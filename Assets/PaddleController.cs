f0fcfb8d56499dec5f1aa60b49afe52aabc1eb47
Declan <mccabed7@tcd.ie>
2024-01-16 23:11:24 +0000

    Basic paddle movement script done

--- Assets/PaddleController.cs
+++ Assets/PaddleController.cs
@@ -2,45 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
 
 public class PaddleController : MonoBehaviour
 {
+    public float speed = 2f;
+    public float minX = 0; // remember to switch stuff to mobile
+    public float maxX = 5;
 
-    // TODO Complete logic in fixedUpdate for using moveToward.  Use random generator for destinations.
+    private Vector2 targetPosition;
 
-    public Transform transformComponent;
-    public Rigidbody2D myRigidbody;
-    public float speed = 1f;
-
-    private float destination;
-    private Vector2 direction;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        throw new System.Exception("PaddleController not testable yet.  See TODO section in PaddleController.cs");
-        if ( speed < 0f ) { throw new System.Exception("Error, speed cannot be negative!"); }
-
-        direction = Vector2.up * speed;
-        
+        PickNewTarget();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+
+    private void Update()
     {
-        
+        MovePaddle();
+
+        if ((Vector2)transform.position == targetPosition)
+        {
+            PickNewTarget();
+        }
+
     }
 
-    void moveToward(float yDest)
+    private void MovePaddle()
     {
-        if(transformComponent.position.y < yDest)
-        {
-            direction.y = 1 * speed;
-        }
-        else
-        {
-            direction.y = -1 * speed;
-        }
-        myRigidbody.MovePosition((Vector2)transformComponent.position + direction);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+    }
+
+    private void PickNewTarget()
+    {
+        float randomX = Random.Range(minX, maxX);
+        targetPosition = new Vector2(randomX, transform.position.y);
     }
 }
+
+
